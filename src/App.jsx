cedca6af1f2b6730961c94e5099e53bd1a0e3855
Gd2r <Mr9r9or@gmail.com>
2025-02-13 13:57:24 +0300

    fixed the site link

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,18 @@ function App() {
   return (
     <>
       <div>
-        <a href="HishamAlahamdi.com" target="_blank">
+        <a href="https://hishamalahmadi.com/" target="_blank">
           <img src={ElonGif} className="Elon" alt="ElonMusk Gif" />
         </a>
       </div>
       <h1>Hello React!</h1>
+
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
+
       <p className="read-the-docs">Click on Elon to go to Hisham&apos;s Site</p>
     </>
   );
